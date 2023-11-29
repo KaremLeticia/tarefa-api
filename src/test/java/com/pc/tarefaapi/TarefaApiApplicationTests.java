45dd494cf87832e643492ea9797bbddf0ac1d5a6
Karem Leticia <karemsantosleticia@gmail.com>
2023-11-28 23:20:41 -0300

    Initial commit

---
+++ src/test/java/com/pc/tarefaapi/TarefaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pc.tarefaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
